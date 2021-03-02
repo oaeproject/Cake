6f825acfd17ac369c7df563ce48f881691370968
RitaOak <oak.rita@hotmail.com>
2021-03-02 17:10:21 +0000

    fix: removed placeholder links and list from third dashboard column (right)

--- src/components/oae-dashboard/oae-dashboard.tsx
+++ src/components/oae-dashboard/oae-dashboard.tsx
@@ -26,15 +26,6 @@ export class Dashboard {
         </div>
         <div class="column options">
           <div class="menu sticky">
-            <p class="menu-label"><b>Quick Links</b></p>
-            <ul id="menu" class="menu-list">
-              <li><a href="#basic">Basic SEO Checklist</a></li>
-              <li><a href="#keyword">Keyword Research Checklist</a></li>
-              <li><a href="#onpage">On-Page SEO Checklist</a></li>
-              <li><a href="#content">Content Checklist</a></li>
-              <li><a href="#technical">Technical SEO Checklist</a></li>
-              <li><a href="#link">Link Building Checklist</a></li>
-            </ul>
           </div>
         </div>
       </oae-layout>

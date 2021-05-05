b11924a6c9df4ae0b8dad376cac9d51b80399e1d
Miguel Laginha <me@miguellaginha.com>
2021-05-05 09:58:51 +0100

    feat: removed empty objects from converted translations

--- src/scripts/convert-properties-to-json.js
+++ src/scripts/convert-properties-to-json.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 
 /**
- * TODO
- * [x] Create optimist cli interface
- * [x] Make locale an input
- * [ ] Add tests
+ * How to run:
+ * node -r esm src/scripts/convert-properties-to-json.js --locale=<locale>
  */
 
 const humanize = require("@jsdevtools/humanize-anything");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
-const argv = yargs(hideBin(process.argv)).argv;
 const fs = require("fs");
 const Path = require("path");
 const util = require("util");
@@ -34,8 +31,15 @@ const {
   objOf,
   concat,
   ifElse,
+  isEmpty,
+  forEachObjIndexed,
+  is,
+  reject,
 } = require("ramda");
 
+const isNotEmpty = pipe(isEmpty, not);
+const isAnObject = is(Object);
+const removeEmptyEntries = reject(isEmpty);
 const doesNotInclude = pipe(includes, not);
 const promiseToReadFile = util.promisify(fs.readFile);
 const splitLineByLine = pipe(toString, split("\n"));
@@ -52,68 +56,66 @@ const extractKeyValuePairs = map((eachLine) => {
  * Eventually we'll be able to point it to a crowdin downloaded
  * folder when it includes en_GB translations as well
  */
-const FOLDER_PATH = Path.join(
-  process.cwd(),
-  "..",
-  "..",
-  "Hilary",
-  "3akai-ux"
-);
+const FOLDER_PATH = Path.join(process.cwd(), "..", "..", "Hilary", "3akai-ux");
 
 const ENGLISH_UK = "en_GB";
 const FRENCH = "fr_FR";
 const PORTUGUESE = "pt_PT";
 const ALLOWED_LOCALES = [ENGLISH_UK, FRENCH, PORTUGUESE];
-
 const NAME = "name";
 const ALLOWED_ROOT_FOLDERS = ["packages", "shared"];
 
+const isUkEnglish = equals(ENGLISH_UK);
+const addPropertiesExtension = concat(__, ".properties");
+const returnDefaultPropertiesFile = () => addPropertiesExtension("default");
+const returnLocalePropertiesFile = addPropertiesExtension;
+
 const pickOneAvailable = `Pick one of ${humanize.values(ALLOWED_LOCALES)}`;
-const isIllegalLocale = doesNotInclude(argv.locale, ALLOWED_LOCALES);
-const isLocaleNotDefined = pipe(prop("locale"), isNil)(argv);
+const isIllegalLocale = () => doesNotInclude(__, ALLOWED_LOCALES);
+const isLocaleNotDefined = isNil;
 const errorOut = (message) => {
   console.error(message);
   process.exit(1);
 };
 
-if (isLocaleNotDefined) {
+/**
+ * CLI input parsing
+ */
+const argv = yargs(hideBin(process.argv)).argv;
+const { locale } = argv;
+
+if (isLocaleNotDefined(locale)) {
   errorOut(
     `Please specify the locale with --locale=<locale>. ${pickOneAvailable}`
   );
-} else if (isIllegalLocale) {
+} else if (isIllegalLocale(locale)) {
   errorOut(
-    `Specified locale (${argv.locale}) is not supported. ${pickOneAvailable}`
+    `Specified locale (${locale}) is not supported. ${pickOneAvailable}`
   );
 }
 
-const isUkEnglish = equals(ENGLISH_UK);
-const addPropertiesExtension = concat(__, ".properties");
-const returnDefaultPropertiesFile = () => addPropertiesExtension("default");
-const returnLocalePropertiesFile = () => addPropertiesExtension(argv.locale);
+const run = (locale, folderPath) => {
+  const LOCALE_JSON_FILE = Path.join(
+    process.cwd(),
+    "src",
+    "i18n",
+    concat(locale, ".json")
+  );
 
-const LOCALE_PROPERTIES_FILE = ifElse(
-  isUkEnglish,
-  returnDefaultPropertiesFile,
-  returnLocalePropertiesFile
-)(argv.locale);
-const LOCALE_JSON_FILE = Path.join(
-  process.cwd(),
-  "src",
-  "i18n",
-  concat(argv.locale, ".json")
-);
+  const getWritableBufferFromObject = (object) =>
+    new Uint8Array(Buffer.from(JSON.stringify(object)));
+  const writeJsonFile = (data) =>
+    util.promisify(fs.writeFile)(LOCALE_JSON_FILE, data);
 
-const isRightLocale = pipe(prop(NAME), startsWith(LOCALE_PROPERTIES_FILE));
-
-convertFolderTreeToObject(FOLDER_PATH, {}, true)
-  .then((rootObject) => {
-    const data = new Uint8Array(Buffer.from(JSON.stringify(rootObject)));
-    return util.promisify(fs.writeFile)(LOCALE_JSON_FILE, data);
-  })
-  .then(() => {
-    console.log(`The JSON file ${LOCALE_JSON_FILE} has been saved. Bye!`);
-  })
-  .catch(console.error);
+  convertFolderTreeToObject(locale, folderPath, {}, true)
+    .then(cleanEmptyEntries)
+    .then(getWritableBufferFromObject)
+    .then(writeJsonFile)
+    .then(() => {
+      console.log(`The JSON file ${LOCALE_JSON_FILE} has been saved. Bye!`);
+    })
+    .catch(console.error);
+};
 
 async function extractKeyValuePairsFrom(filePath) {
   return promiseToReadFile(filePath)
@@ -123,8 +125,38 @@ async function extractKeyValuePairsFrom(filePath) {
     .catch(console.error);
 }
 
-async function convertFolderTreeToObject(folderPath, rootObject, root = false) {
+const cleanEmptyEntries = (object) => {
+  let newObject = {};
+
+  forEachObjIndexed((value, key) => {
+    const keyLens = lensProp(key);
+    const isNestedEntry = and(isAnObject(value), isNotEmpty(value));
+
+    switch (true) {
+      case isNestedEntry:
+        value = cleanEmptyEntries(value);
+      default:
+        newObject = set(keyLens, value, newObject);
+        break;
+    }
+  }, object);
+
+  return removeEmptyEntries(newObject);
+};
+
+async function convertFolderTreeToObject(
+  locale,
+  folderPath,
+  rootObject,
+  root = false
+) {
   const folderChildren = await fs.promises.opendir(folderPath);
+  const localePropertiesFile = ifElse(
+    isUkEnglish,
+    returnDefaultPropertiesFile,
+    returnLocalePropertiesFile
+  )(locale);
+  const isRightLocale = pipe(prop(NAME), startsWith(localePropertiesFile));
 
   for await (const eachFolderChild of folderChildren) {
     const childPath = Path.join(folderPath, eachFolderChild.name);
@@ -133,16 +165,17 @@ async function convertFolderTreeToObject(folderPath, rootObject, root = false) {
       eachFolderChild.isFile(),
       isRightLocale(eachFolderChild)
     );
+
+    /**
+     * When downloading from crowdin, we get all the translations except English
+     * To migrate English translations, one must traverse 3akai-ux instead of crowdin
+     * downloaded folder tree
+     * Because of this, we must exclude 3akai-ux root folders, all except `ALLOWED_FOLDERS`
+     */
     const ifIllegalRootChildren =
       root &&
       isDirectory &&
       doesNotInclude(eachFolderChild.name, ALLOWED_ROOT_FOLDERS);
-
-    /**
-     * When downloading from crowdin, we get all the translations except English
-     * To migrate English translations, one must traverse 3akai-ux instead of crowdin downloaded folder tree
-     * Because of this, we must exclude 3akai-ux root folders, all except `ALLOWED_FOLDERS`
-     */
     if (ifIllegalRootChildren) {
       console.log(`Skipping root folder ${eachFolderChild.name}`);
       continue;
@@ -152,7 +185,7 @@ async function convertFolderTreeToObject(folderPath, rootObject, root = false) {
       case isDirectory:
         rootObject = set(
           lensProp(eachFolderChild.name),
-          await convertFolderTreeToObject(childPath, {}),
+          await convertFolderTreeToObject(locale, childPath, {}),
           rootObject
         );
         break;
@@ -165,3 +198,5 @@ async function convertFolderTreeToObject(folderPath, rootObject, root = false) {
   }
   return rootObject;
 }
+
+run(locale, FOLDER_PATH);

87e58bee2a35e5155247b65f1d2acf79add068f2
Miguel Laginha <me@miguellaginha.com>
2021-07-27 10:47:20 +0100

    chore: minor change

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
-import { register, init, getLocaleFromNavigator } from "svelte-i18n";
+import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
-register("fr_FR", () => import("./i18n/fr_FR.json"));
-register("en_GB", () => import("./i18n/en_GB.json"));
+register('en_GB', () => import('./i18n/en_GB.json'));
+register('fr_FR', () => import('./i18n/fr_FR.json'));
 
 init({
-  fallbackLocale: "fr_FR",
+  fallbackLocale: 'fr_FR',
   initialLocale: getLocaleFromNavigator(),
 });
